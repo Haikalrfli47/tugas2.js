7520d9815a8131803780284099f685f7d106e3c0
Haikal Rafli <haikalraflis35@gmail.com>
2021-11-13 12:05:18 +0700

    TUGAS 2 JS
    tugas kedua javascript

---
+++ tugas2.js
@@ -0,0 +1,6 @@
+var number = 11;
+var string = 'wahyu';
+var boolean = true;
+var undefined = undefined;
+var Null = null;
+console.log(`number : ${number}\nstring : ${string}\nboolean : ${boolean}\nundefined : ${undefined}\nnull : ${null} `);
